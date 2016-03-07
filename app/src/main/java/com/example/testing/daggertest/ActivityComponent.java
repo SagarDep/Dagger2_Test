6a80f5778cea50a324067435f80770bd1f7d912f
sangcomz <sangcomz@naver.com>
2016-03-07 19:11:56 +0900

    fix

--- app/src/main/java/com/example/testing/daggertest/ActivityComponent.java
+++ app/src/main/java/com/example/testing/daggertest/ActivityComponent.java
@@ -1,12 +1,14 @@
 package com.example.testing.daggertest;
 
+import com.example.testing.daggertest.scope.PerActivity;
+
 import dagger.Component;
 
 /**
  * Created by sangcomz on 16. 3. 7.
  */
 
-
+@PerActivity
 @Component(
         modules = {
         ActivityModule.class
