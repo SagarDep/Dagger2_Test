8d643727c7ecfc3e44ba7d6fb265ed7a0f18d3f7
sangcomz <sangcomz@naver.com>
2016-03-09 16:54:16 +0900

    fix

--- app/src/main/java/com/example/testing/daggertest/UserModel.java
+++ app/src/main/java/com/example/testing/daggertest/UserModel.java
@@ -11,21 +11,6 @@ public class UserModel {
     UserModel(){
         name = "Seok Won";
         Log.d("UserModel","My Name is " + name);
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    for (int i = 0; i < 100; i++) {
-                        System.out.println("UserModel thread!" + i );
-                        Thread.sleep(2000);
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
-        t.start();
     }
 
     public String getName() {
